58d4259949c5241159b2e69041cce7f895cf769e
Jonathan Guerne <jonathan.guerne@he-arc.ch>
2016-08-22 10:24:06 +0200

    remise a jour

---
+++ programme principale/projet_p1/Form_test_dessin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projet_p1
+{
+    public partial class Form_test_dessin : Form
+    {
+        public Form_test_dessin()
+        {
+            InitializeComponent();
+        }
+    }
+}
